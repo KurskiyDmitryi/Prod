30936b1e01900396c31ae7af6eef4264238e54f7
dima <dima.it.1989@gmail.com>
2023-01-15 14:48:43 +0100

    change css view_one blade

--- resources/views/blog/view_one.blade.php
+++ resources/views/blog/view_one.blade.php
@@ -4,7 +4,9 @@
 @endsection
 @section('content')
     <h1 style="text-align: center; color: skyblue; margin-top: 50px">Title :: {{$blog->title}}</h1>
-    <h2 style="text-align: center; color: skyblue; margin-top: 10px;">Content :: {{$blog->content}}</h2>
-    @if(!empty($blog->ps))<h1 style="text-align: center; color: skyblue; margin-top: 50px">P.S. :: {{$blog->ps}}</h1>@endif
+    <h2 style="text-align:center; color: skyblue; margin-top: 10px; width: 500px">Content : </h2><p style="margin-left: 15px">{{$blog->content}}</p>
+    @if(!empty($blog->ps))
+        <h2 style="text-align:center; color: skyblue; margin-top: 50px; width: 500px">P.S. :</h2> <p style="margin: 15px">{{$blog->ps}}</p>
+    @endif
     <h6 style="text-align: center; margin-top: 80px">Created :: {{$blog->created_at}}</h6>
 @endsection
