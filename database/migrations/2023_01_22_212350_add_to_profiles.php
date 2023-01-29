7cb7dccd47e9c8bab90fff3b880032ba1e3149b2
dima <dima.it.1989@gmail.com>
2023-01-29 20:53:34 +0100

    fix profile and index profile page

--- database/migrations/2023_01_22_212350_add_to_profiles.php
+++ database/migrations/2023_01_22_212350_add_to_profiles.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('profiles', function (Blueprint $table) {
-            $table->string('type')->after('sex');
+            $table->string('type')->nullable()->after('sex');
         });
     }
 
